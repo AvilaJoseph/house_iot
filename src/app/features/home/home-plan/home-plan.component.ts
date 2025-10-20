be3f96a815a3998142e3c5499059545eab32c1f1
AvilaJoseph <avilajoseph2021@gmail.com>
2025-10-19 22:15:21 -0500

    Implementación de los puntos del AA en el mapa de la casa

--- src/app/features/home/home-plan/home-plan.component.ts
+++ src/app/features/home/home-plan/home-plan.component.ts
@@ -14,6 +14,8 @@ interface Room {
   name: string;
   lights: Light[];
   ac?: boolean;
+  acX?: number;
+  acY?: number;
 }
 
 @Component({
@@ -25,50 +27,58 @@ interface Room {
 })
 export class HomePlanComponent {
   hoveredLight: string | null = null;
+  hoveredAC: string | null = null;
 
   rooms: Room[] = [
     {
-      id: 'entrada',
-      name: 'Entrada',
+      id: 'Baño',
+      name: 'Baño',
       lights: [
-        { id: 'l1', label: 'Luz 1', on: false, x: 103, y: 142 },
+        { id: 'l1', label: 'Luz 1', on: false, x: 670, y: 260 },
+        { id: 'l2', label: 'Luz 2', on: false, x: 223, y: 290 },
       ]
-    },
-    {
-      id: 'sala',
-      name: 'Sala',
-      lights: [
-        { id: 'l4', label: 'Luz 4', on: false, x: 195, y: 285 },
-        { id: 'l5', label: 'Luz 5', on: true, x: 365, y: 335 }
-      ]
-    },
-    {
-      id: 'cocina',
-      name: 'Cocina',
-      lights: [{ id: 'l1', label: 'Luz 1', on: false, x: 235, y: 220 }]
     },
     {
       id: 'habitacion1',
       name: 'Habitación 1',
       lights: [{ id: 'l1', label: 'Luz 1', on: false, x: 627, y: 113 }],
-      ac: false
+      ac: false,
+      acX: 627,
+      acY: 88
     },
     {
       id: 'habitacion2',
       name: 'Habitación 2',
-      lights: [{ id: 'l1', label: 'Luz 1', on: true, x: 279, y: 112 }],
-      ac: true
+      lights: [{ id: 'l1', label: 'Luz 1', on: true, x: 450, y: 134 }],
+      ac: true,
+      acX: 450,
+      acY: 109
     },
     {
       id: 'habitacion3',
       name: 'Habitación 3',
-      lights: [{ id: 'l1', label: 'Luz 1', on: false, x: 670, y: 260 }],
-      ac: false
+      lights: [{ id: 'l1', label: 'Luz 1', on: true, x: 279, y: 112 }],
+      ac: true,
+      acX: 297,
+      acY: 45
     },
     {
-      id: 'baño',
-      name: 'Baño',
-      lights: [{ id: 'l1', label: 'Luz 1', on: false, x: 655, y: 395 }]
+      id: 'Habitacion4',
+      name: 'Habitación 4',
+      lights: [
+        { id: 'l1', label: 'Luz 1', on: false, x: 103, y: 142 },
+      ],
+      ac: false,
+      acX: 108,
+      acY: 45
+    },
+    {
+      id: 'habitacionFinal',
+      name: 'entrada',
+      lights: [
+        { id: 'l1', label: 'Luz 1', on: true, x: 750, y: 95 },
+        { id: 'l2', label: 'Luz 2', on: false, x: 750, y: 470 }
+      ]
     },
     {
       id: 'patio',
@@ -76,14 +86,13 @@ export class HomePlanComponent {
       lights: [{ id: 'l1', label: 'Luz 1', on: true, x: 385, y: 470 }]
     },
     {
-      id: 'habitacionFinal',
-      name: 'Entrada',
+      id: 'sala',
+      name: 'Sala',
       lights: [
-        { id: 'l1', label: 'Luz 1', on: false, x: 720, y: 115 },
-        { id: 'l2', label: 'Luz 2', on: true, x: 750, y: 95 },
-        { id: 'l3', label: 'Luz 3', on: false, x: 780, y: 115 }
-      ],
-      ac: false
+        { id: 'l3', label: 'Luz 3', on: true, x: 380, y: 309 },
+        { id: 'l2', label: 'Luz 2', on: false, x: 522, y: 311 },
+        { id: 'l1', label: 'Luz 1', on: false, x: 655, y: 378 }
+      ]
     }
   ];
 
