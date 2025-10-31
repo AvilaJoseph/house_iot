5f2d92a04684c6604a347b7371bc20164ade1a95
AvilaJoseph <avilajoseph2021@gmail.com>
2025-10-30 19:24:29 -0500

    Correciones

--- src/app/features/devices/devices.component.ts
+++ src/app/features/devices/devices.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TopbarDevices } from "./topbar-devices/topbar-devices.component";
 import {  ButtonsListComponent } from "./buttons-list/buttons-list.component";
+import { ListDevices } from './list-devices/list-devices.component';
 
 @Component({
   selector: 'devices-page',
-  imports: [TopbarDevices, ButtonsListComponent],
+  imports: [TopbarDevices, ButtonsListComponent, ListDevices],
   templateUrl: './devices.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/features/devices/list-devices/list-devices.component.ts
+++ src/app/features/devices/list-devices/list-devices.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'devices-list-devices',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-devices.component.html',
   styleUrls: ['./list-devices.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListDevices { }
+export class ListDevices {
+  DevicesList = [
+    { name: 'Device 1', type: 'Sensor', udid: '12345', serialNumber: 'SN12345', status: 'Active', location: 'Living Room' },
+    { name: 'Device 2', type: 'Camera', udid: '67890', serialNumber: 'SN67890', status: 'Inactive', location: 'Front Door' },
+    { name: 'Device 3', type: 'Thermostat', udid: '54321', serialNumber: 'SN54321', status: 'Active', location: 'Bedroom' },
+  ]
+ }
