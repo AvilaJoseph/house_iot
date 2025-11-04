281ce562c1e4952dab42f4f0f01e53320de2a26c
AvilaJoseph <avilajoseph2021@gmail.com>
2025-11-04 14:42:59 -0500

    Correciones

--- src/app/features/security/security.component.ts
+++ src/app/features/security/security.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ListSecurity } from "./list-security/list-security.component";
 import { ListAds } from "./list-ads/list-ads.component";
 import { SecurityPlanComponent } from "./camera-maps/camera-maps.component";
+import { Addbutton } from "../../shared/components/addbutton/addbutton.component";
 
 @Component({
   selector: 'home-security',
-  imports: [ListSecurity, ListAds, SecurityPlanComponent],
+  imports: [ListSecurity, ListAds, SecurityPlanComponent, Addbutton],
   templateUrl: './security.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
